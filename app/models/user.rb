b5cd06d6cd37792628e7bb5fa8e47c54f7595771
theboymo <billfero@gmail.com>
2017-09-26 15:12:40 +0100

    impl user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,1 +1,10 @@
-# user model
+class User < ActiveRecord::Base
+  has_many :shelves
+
+  validates :username, uniqueness: true
+  validates :username, presence: true
+  validates :email, uniqueness: true
+  validates :email, presence: true
+  validates :password, presence: true
+  
+end
