2f5a588ea65b2f2bea36dc982b0d022e2aca70da
theboymo <billfero@gmail.com>
2017-10-11 19:30:46 +0100

    update shelves controller to handle invalid users and stop current user accessing another users book shelf

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -2,6 +2,14 @@ class ShelvesController < ApplicationController
 
   get '/shelves/:slug' do
     @user = User.find_by_slug(params[:slug])
+    if !@user
+      not_found
+    else
+      # redirect user to home when trying to access another user's book shelf
+      if @user.id != current_user.id
+        redirect :'/'
+      end
+    end
     erb :'shelves/index'
   end
 end
