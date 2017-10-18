3c8fcbee6927a6bb041f6feb27851ceb7f6c5d6e
theboymo <billfero@gmail.com>
2017-10-18 15:54:30 +0100

    refactor shelves controller, check if the currnet user is authorised to view the current shelf

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -1,17 +1,21 @@
 class ShelvesController < ApplicationController
 
   get '/shelves/:slug' do
-    @user = User.find_by_slug(params[:slug])
-    if !@user
-      not_found
-    else
-      # REVIEW: 
-      # This looks weird, let's talk about it
-      # redirect user to home when trying to access another user's book shelf
-      if @user.id != current_user.id
-        redirect :'/'
-      end
-    end
+    redirect_if_unauthorised && return
+
+    @user = current_user
     erb :'shelves/index'
   end
+
+  private
+
+  def redirect_if_unauthorised
+    if params[:slug] != current_user.slug
+      redirect :'/'
+      true
+    else
+      false
+    end
+  end
+
 end
